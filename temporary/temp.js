92a7d61d6bfd982f9aa2f9d936e1de79df66abad
not-the <87151784+not-the@users.noreply.github.com>
2024-01-10 02:03:05 -0500

    Replaced Google Fonts with local

--- temporary/temp.js
+++ temporary/temp.js
@@ -242,11 +242,7 @@ function populatePage(id, fullpage=false) {
     <link rel="stylesheet" href="https://not-the.github.io/not-util/not-util.css" crossorigin>
     <script src="https://not-the.github.io/not-util/not-util.js" crossorigin></script>
 
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-
+    
 </head>
 <body ${item.light ? 'class="light_header"' : ''}>
     <!---------- Navigation ---------->
